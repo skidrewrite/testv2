30a47f8ca2b2cb4c8261f7a7b20a4bf0ef2b5034
R12sa <144062428+R12sa@users.noreply.github.com>
2026-05-24 14:44:20 -0700

    Update reinstall.lua to point to R12sa repository and clean up messaging

--- reinstall.lua
+++ reinstall.lua
@@ -1,6 +1,6 @@
 local folderToClean = "newvape"
 local folderToKeep = "profiles"
-local reinstallUrl = "https://raw.githubusercontent.com/poopparty/poopparty/main/NewMainScript.lua"
+local reinstallUrl = "https://raw.githubusercontent.com/R12sa/R12SAVapeV4/main/NewMainScript.lua"
 
 local function deleteRecursive(path, keepPath)
     if path == keepPath then return end
@@ -23,7 +23,7 @@ end
 
 local keepFullPath = folderToClean .. "/" .. folderToKeep
 if not isfolder(keepFullPath) then
-    print("warning nigga: '" .. keepFullPath .. "' missing  - everything boutta get deleted gang")
+    print("warning: '" .. keepFullPath .. "' missing - everything is about to get deleted")
 end
 
 for _, item in ipairs(listfiles(folderToClean)) do
@@ -32,6 +32,6 @@ for _, item in ipairs(listfiles(folderToClean)) do
     end
 end
 
-print("loading aerov4...")
-task.wait(1) --no no crash dabo
+print("loading R12SA V4...")
+task.wait(1)
 loadstring(game:HttpGet(reinstallUrl, true))()
